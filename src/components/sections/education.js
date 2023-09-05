7756617cf08b25645732bf8dbffb68d67a4e88e7
Martin Caraballo <martin@martin.martin>
2023-09-04 23:15:34 -0300

    work on sections

--- src/components/sections/education.js
+++ src/components/sections/education.js
@@ -5,8 +5,13 @@ export default function Education() {
         <div className={"section"}>
             <div className="section-content" id={"education"}>
                 <h1>Datos académicos</h1>
-                <p>Graduado de bachillerato en Ingeniería, cursado en Liceo N°1 Eduardo Fabini, ubicado en Minas, Lavalleja.</p>
-                <p>Estudiante en Universidad Católica del Urugay, Plan 2021.</p>
+                <p>Graduado de bachillerato en Ingeniería, cursado en Liceo N°1 Eduardo Fabini, ubicado en Minas,
+                    Lavalleja.</p>
+                <p>También terminé dos cursos en la plataforma "Conecta Empleo" de Movistar: <br></br>
+                    - Curso de introducción a la Programación. <br></br>
+                    - Curso: “Creando Apps en Android”.</p>
+                <p>Actualmente, me encuentro estudiando Ingeniería en Informática, en la Universidad Católica del
+                    Uruguay, Plan 2021.</p>
             </div>
         </div>
     );
--- src/components/sections/skills.js
+++ src/components/sections/skills.js
@@ -5,7 +5,10 @@ export default function Skills() {
         <div className={"section section-dark"}>
             <div className="section-content" id={"skills"}>
                 <h1>Habilidades</h1>
-                <p>sadasdsadasdsadad</p>
+                <p>A lo largo de mi carrera he adoptado habilidades en diferentes lenguajes de programación, estructuras
+                    de datos, desarrollo de algoritmos, optimización; pero no solo habilidades de programación o diseño,
+                    sino que también de trabajo en equipo, comunicación, autocrítica y aprendizaje.</p>
+                <p>Tecnologías en las que tengo más experiencia:</p>
             </div>
         </div>
     );
